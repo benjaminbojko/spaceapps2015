199c435e659ae1f6770e2e73a40dbfa1d8d81305
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2015-04-12 13:03:04 -0400

    Stacey adding the Unity sound test

---
+++ tests/UnitySoundTest/AudioTest/Assets/scripts/BackgroundSound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.Audio;
+
+
+
+public class BackgroundSound : MonoBehaviour {
+
+	public string url;
+	AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		WWW www = new WWW (url);
+
+		source = GetComponent<AudioSource> ();
+		source.clip = www.audioClip;
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (source.clip);
+		Debug.Log (source.clip.isReadyToPlay);
+		Debug.Log (source.clip.loadState);
+		if(!source.isPlaying && source.clip.isReadyToPlay)
+			source.Play();
+	}
+}
+
+
+
+
